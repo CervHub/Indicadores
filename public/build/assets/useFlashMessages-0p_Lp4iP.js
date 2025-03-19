d717677936ddbf7f1e57f2b1a2201f389731f825
MoisesSalazar <moises.salazar@ucsp.edu.pe>
2025-03-19 14:45:28 -0500

    Add vistas

--- public/build/assets/useFlashMessages-0p_Lp4iP.js
+++
@@ -1,1 +0,0 @@
-import{K as c,r as s}from"./app-dQT239ps.js";import{t as o}from"./index-CDdN-Nje.js";function i(){const{flash:r,error:e}=c().props;s.useEffect(()=>{r!=null&&r.success&&o.success(r.success)},[r==null?void 0:r.success]),s.useEffect(()=>{r!=null&&r.error&&o.error(r.error)},[r==null?void 0:r.error]),s.useEffect(()=>{e&&o.error(e)},[e])}export{i as u};
