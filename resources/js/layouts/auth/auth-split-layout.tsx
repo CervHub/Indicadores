5246752b5570eb4e8adc75f2003c6832584b3b82
MoisesSalazar <moises.salazar@ucsp.edu.pe>
2025-04-02 08:41:54 -0500

    Vistas

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -7,7 +7,7 @@ interface AuthLayoutProps {
 
 export default function AuthSplitLayout({ children }: PropsWithChildren<AuthLayoutProps>) {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center p-6 md:p-10 bg-muted md:bg-none bg-mobile relative">
+        <div className="flex min-h-screen flex-col items-center justify-center p-6 md:p-10 bg-mobile relative">
             {/* Overlay para el efecto oscuro */}
             <div className="absolute inset-0 bg-black/50 md:hidden"></div>
             <div className="relative w-full max-w-sm md:max-w-4xl">{children}</div>
