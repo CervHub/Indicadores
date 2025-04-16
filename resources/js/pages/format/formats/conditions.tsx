0638206436b64eeca2b92fdb44da88fc5dbe4908
Moises_Salazar <moises.salazar@ucsp.edu.pe>
2025-04-16 18:30:32 -0500

    Finalizando

--- resources/js/pages/format/formats/conditions.tsx
+++ resources/js/pages/format/formats/conditions.tsx
@@ -1,6 +1,9 @@
+import TemplateForm from '@/components/form/template';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react'; // Importamos Link de Inertia.js
+import { Head } from '@inertiajs/react';
+
+const DEFAULT_COORDINATES = { lat: -17.270662, lng: -70.617886 };
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Gestión de Formatos',
@@ -12,11 +15,31 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+const gerencias = [
+    { id: 'gerencia1', name: 'Gerencia de Operaciones' },
+    { id: 'gerencia2', name: 'Gerencia de Recursos Humanos' },
+    { id: 'gerencia3', name: 'Gerencia de Finanzas' },
+];
+
+const empresas = [
+    { id: 'empresa1', name: 'Empresa A' },
+    { id: 'empresa2', name: 'Empresa B' },
+    { id: 'empresa3', name: 'Empresa C' },
+];
+
+const causas = [
+    { id: 'causa1', name: 'Causa 1' },
+    { id: 'causa2', name: 'Causa 2' },
+    { id: 'causa3', name: 'Causa 3' },
+];
+
 export default function Conditions() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Condiciones subestándar" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4"></div>
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <TemplateForm defaultCoordinates={DEFAULT_COORDINATES} gerencias={gerencias} empresas={empresas} causas={causas} />
+            </div>{' '}
         </AppLayout>
     );
 }
--- resources/js/pages/format/formats/incidents.tsx
+++ resources/js/pages/format/formats/incidents.tsx
@@ -1,6 +1,10 @@
+import TemplateForm from '@/components/form/template';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react'; // Importamos Link de Inertia.js
+import { Head } from '@inertiajs/react';
+
+const DEFAULT_COORDINATES = { lat: -17.270662, lng: -70.617886 };
+
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Gestión de Formatos',
@@ -12,11 +16,30 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+const gerencias = [
+    { id: 'gerencia1', name: 'Gerencia de Operaciones' },
+    { id: 'gerencia2', name: 'Gerencia de Recursos Humanos' },
+    { id: 'gerencia3', name: 'Gerencia de Finanzas' },
+];
+
+const empresas = [
+    { id: 'empresa1', name: 'Empresa A' },
+    { id: 'empresa2', name: 'Empresa B' },
+    { id: 'empresa3', name: 'Empresa C' },
+];
+
+const causas = [
+    { id: 'causa1', name: 'Causa 1' },
+    { id: 'causa2', name: 'Causa 2' },
+    { id: 'causa3', name: 'Causa 3' },
+];
 export default function Incidents() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Incidentes" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4"></div>
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <TemplateForm defaultCoordinates={DEFAULT_COORDINATES} gerencias={gerencias} empresas={empresas} causas={causas} />
+            </div>{' '}
         </AppLayout>
     );
 }
