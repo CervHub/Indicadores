09efdef106f0053f03710d11ffeda7d0c80c4a12
Moises_Salazar <moises.salazar@ucsp.edu.pe>
2025-04-16 12:28:49 -0500

    update gitignore

--- copy_untracked_files.sh
+++ copy_untracked_files.sh
@@ -4,13 +4,16 @@
 current_date=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Crear la carpeta de destino con el nombre dinámico dentro de la carpeta principal "Production"
-destination_folder="Production/SouthernCuajoneGestionSST_$current_date"
+destination_folder="Production/SouthernToquepalaGestionSST_$current_date"
 mkdir -p "$destination_folder"
 
-# Encontrar y copiar los archivos modificados y agregados en el remoto en los últimos 15 días
+# Encontrar y copiar los archivos modificados y agregados desde el 8 de abril de 2025 hasta la fecha actual
 # Manteniendo la estructura de directorios, ignorando "resources/js"
 git fetch origin
-git diff --name-only --diff-filter=MA "@{1 days ago}" HEAD | grep -v '^resources/js/' | xargs -I % cp --parents % "$destination_folder/"
+git diff --name-only --diff-filter=MA "2025-04-08" HEAD | grep -v '^resources/js/' | xargs -I % cp --parents % "$destination_folder/"
+
+# Copiar siempre el contenido de la carpeta public/build
+cp -r public/build "$destination_folder/public/build"
 
 # Mostrar el estado de los archivos
 git status
