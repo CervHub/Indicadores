b2bf905606ad41210aa18b796faeb7535c81beaa
unknown <moises.salazar@cerv.com.pe>
2025-05-14 11:32:41 -0500

    Finalizado

--- public/build/assets/input-error-hh2-eUbX.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CEM5Qn-0.js";import{d as o}from"./utils-B6AGoeaF.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
